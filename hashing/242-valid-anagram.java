f535de5deedb384ba53b1968ab197bbb49e0b0da
Sidharth <sidbangera.comms@gmail.com>
2026-02-23 19:49:40 +0530

    Pushed Valid Anagram

---
+++ hashing/242-valid-anagram.java
@@ -0,0 +1,25 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        Map<Character,Integer> map1 = new HashMap<>();
+        Map<Character,Integer> map2 = new HashMap<>();
+
+        if(s.length() != t.length())
+        {
+            return false;
+        }
+
+        for(char c:s.toCharArray())
+        {
+            map1.put(c,map1.getOrDefault(c,0)+1);
+        }
+
+        for(char c:t.toCharArray())
+        {
+            map2.put(c,map2.getOrDefault(c,0)+1);
+        }
+
+        return map1.equals(map2);
+
+
+    }
+}
