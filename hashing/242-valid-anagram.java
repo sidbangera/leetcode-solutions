80a766583a3f3d72975725deded731f9e0fe2acd
Sidharth <sidbangera.comms@gmail.com>
2026-06-20 10:30:33 +0530

    Solved Reverse Linked List

--- hashing/242-valid-anagram.java
+++ hashing/242-valid-anagram.java
@@ -23,3 +23,31 @@ class Solution {
 
     }
 }
+
+
+/*
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        HashMap<Character,Integer> map = new HashMap<>();
+        if(s.length() != t.length()) return false;
+        for(char ch : s.toCharArray()){
+            map.put(ch,map.getOrDefault(ch,0)+1);
+        }
+        for(char ch : t.toCharArray()){
+            if(!map.containsKey(ch)) return false;
+
+            int count = map.get(ch);
+
+            if(count == 1){
+                map.remove(ch);
+            }
+            else
+            {
+                map.put(ch,map.get(ch)-1);
+            }
+        }
+
+        return map.isEmpty();
+    }
+}
+*/
