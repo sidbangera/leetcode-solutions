022f24bd2e19ee50c4ebaa2e2ea1398af142dbd6
Sidharth <sidbangera.comms@gmail.com>
2026-07-22 22:51:34 +0530

    Solved Balanced Binary Tree

---
+++ trees/110-balanced-binary-tree.java
@@ -0,0 +1,20 @@
+class Solution {
+    public boolean isBalanced(TreeNode root) {
+        return height(root) != -1;
+        
+    }
+    int height(TreeNode root){
+        if(root == null) return 0;
+        int leftHeight = height(root.left);
+        if (leftHeight == -1)
+            return -1;
+        int rightHeight = height(root.right);
+        if (rightHeight == -1)
+            return -1;
+        if (Math.abs(leftHeight-rightHeight)>1){
+            return -1;
+        } 
+            
+        return Math.max(leftHeight,rightHeight)+1;
+    }
+}
